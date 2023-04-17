8e397c7e2640767fdb21c23ebd519f3be0aa368f
Diarassouba Mamadou <couda.dm@gmail.com>
2023-04-17 16:59:29 +0000

    diar

--- 3. Pokedex/app.js
+++ 3. Pokedex/app.js
@@ -53,9 +53,9 @@ function fetchPokemonComplet(pokmon) {
         .then((pokeDoke) => {
           ObjPokemonFull.name = pokeDoke.names[4].name;
           AllPokemon.push(ObjPokemonFull); // Ajout dans le tableau pour les affiches sur le DOM
-
+          ;
           if (AllPokemon.length === 151) {
-            //   console.log(AllPokemon)
+              console.log(AllPokemon)
             // trier le tableau des pokemon en utilisant leur Id
 
             tableauFin = AllPokemon.sort((a, b) => {
@@ -103,13 +103,60 @@ window.addEventListener('scroll', function(e){
   // srcollHeigth = scroll total
   // clientHeigth = hauteur de la fenêtre , partie visible.
     console.log(scrollTop , scrollHeight , clientHeight);
+    if (clientHeight + scrollTop >= scrollHeight - 20) { // Si la partie (hauteur de la fenêtre , partie visible) + scroll depuis le top > hauteur  total
+        addPOke(6)
+    }
 })
-// Anilation input 
+
+let index = 21;
+// Fonction qui permet d'ajouter 6 nouvelles carte au 21 premiers
+function addPOke(nb){
+  if(index > 151){ // l'index est superieur au nbr total de pokemon
+    return;
+  } 
+  const arrToArd = AllPokemon.slice(index, index+nb)
+  createCard(arrToArd)
+  
+  index+= nb
+}
+
+
+
+
+// Recherche
+
+  search.addEventListener('keyup', recherche)
+
+    function recherche(){
+      if (index < 151) {
+        addPOke(130)
+      }
+
+        let filter , allLi, titleValue , allTitles;
+
+        filter = search.value.toUpperCase()
+        console.log(filter)
+        allLi = document.querySelectorAll('li')
+        allTitles = document.querySelectorAll('li > h5')
+
+        for (let i = 0; i < allLi.length; i++){
+          titleValue = allTitles[i].innerText;
+          console.log(titleValue)
+         if(titleValue.toUpperCase().indexOf(filter) > -1){
+            allLi[i].style.display = "flex"
+         }else{
+            allLi[i].style.display = "none"
+         }
+          
+        }
+    }
+
+  // Anilation input 
 
 search.addEventListener("input", function (e) {
   if (e.target.value !== "") {
     e.target.parentNode.classList.add("active-input");
-  } else if (e.target.value === "") {
+  } else if (e.target.value === ""){
     e.target.parentNode.classList.remove("active-input");
   }
 });
